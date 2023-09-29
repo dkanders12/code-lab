c7275f3537493263cc5459e7ab81575056d7a781
dkanders12 <dkanders12@gmail.com>
2023-09-29 08:30:15 +0200

    t

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -1,42 +1,49 @@
 /* DOMcelementer */
 
-
-
 /* Opgave 1.1
 Du skal finde elementet med id="redFigure" og console logge det.
 Du skal finde elementet med id="yellowFigure" og console logge det. 
 */
 
 // din kode her
+const redFigure = document.getElementById("redFigure");
 
-
+redFigure.addEventListener("click", () => {
+  redFigure.classList.toggle("blueBackground");
+});
 /* opgave 1.2
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
 farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
 */
+const yellowFigure = document.getElementById("yellowFigure");
 
-
-// din kode her
-
-
-
+yellowFigure.addEventListener("click", () => {
+  yellowFigure.classList.toggle("blueBackground");
+});
 /* opgave 2.1
 Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 - et h2 med teksten: Opgave 2.1 løsning
 - et p med teksten: Jeg har løst opgave 2.1
 */
 
-
 // din kode her
-
 
 // opgave 3 liveHTML lists
 /* opgave 3.1
 Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
 */
-
+const yeet = document.querySelectorAll(".purpleFigures");
+for (let index = 0; index < yeet.length; index++) {
+  console.log(yeet[index]);
+  yeet[index].addEventListener("click", (data) => {
+    changeDiv(data.target);
+  });
+}
+function changeDiv(target) {
+  console.log(target);
+  target.classList.toggle("orgin");
+}
 // din kode her
-
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -46,16 +53,12 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 
 // din kode her
 
-
 /* opgave 3.3
 Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
 myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
 
-
 // din kode her
-
-
 
 /* opgave 4
 opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
@@ -64,14 +67,11 @@ Fyld dine elementers innerHtML eller andre atributter med dine data
 */
 
 const myData = {
-    name: 'tiger',
-    image: 'assets/img/standard_tiger.jpg',
-    description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.'
+  name: "tiger",
+  image: "assets/img/standard_tiger.jpg",
+  description:
+    "En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.",
 };
 
-
 // din kode her
-
-
-
-
+console.log(myData);
